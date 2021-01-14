045970e848edac6e0fa466ea1b5f4756b7d385d7
Maurice Domínguez <maurice.ronet.dominguez@gmail.com>
2021-01-14 14:35:01 +0100

    feat: adds imperative and adt solution

---
+++ adt/index.js
@@ -0,0 +1,37 @@
+const { askInteractive, fetchUsers, saveEmail, handleException, flattenEither, trace } = require('./utils')
+const { pipe, pipeK, prop, map, lift, converge, always, chain, equals } = require('ramda')
+const { find, maybeToEither } = require('crocks')
+const { yellow, green } = require('chalk')
+
+// findUserEmailByName :: String -> [ User ] -> Async (Either Message String)
+const findUserEmailByName = pipe(
+    (name, users) => find(user => user.name.includes(name), users),
+    map(prop('email')),
+    maybeToEither('No exist user email.')
+)
+
+// getUserEmailInteractive :: () -> Async (Either Message Email)
+const getUserEmailInteractive = pipe(
+    converge(lift(findUserEmailByName), [always(askInteractive('Please, introduce name to find Email: ')),  fetchUsers]),
+)
+
+// askSaveEmailInteractive :: Email -> Async (Either Message Email)
+const askSaveEmailInteractive = (email) => pipe(
+    () => askInteractive(`Do you want to save the email "${email}"? `),
+    map(handleException(equals('yes'), email, 'Has decided not to save email!'))
+)()
+
+// saveEmailInteractive :: () -> Async Message Email
+const saveEmailInteractive = pipeK(
+    pipe(getUserEmailInteractive, flattenEither),
+    pipe(askSaveEmailInteractive, flattenEither),
+    saveEmail
+)
+
+// run program
+saveEmailInteractive()
+.fork(
+    trace(yellow('Exception:')), 
+    trace(green('Success::'))
+)
+
---
+++ imperative/index.js
@@ -0,0 +1,41 @@
+const { askInteractive, fetchUsers, saveEmail, trace } = require('./utils')
+const { yellow, green } = require('chalk')
+
+// traceSuccess :: String -> ()
+const traceSuccess = (msg) => trace(green('Success:'), msg)
+
+// traceException :: String -> ()
+const traceException = (msg) => trace(yellow('Exception:'), msg)
+
+// findUserEmailByName :: (String, [ User ]) -> Email | null
+const findUserEmailByName = (name, users) => {
+    const user = users.find(user => user.name.includes(name))
+    return user ? user.email : null
+}
+
+// saveEmailInteractive :: () -> Promise Error ()
+const saveEmailInteractive = async () => {
+    const name = await askInteractive('Please, introduce name to find Email: ')
+    const users = await fetchUsers()
+    const email = findUserEmailByName(name, users)
+
+    if(email){
+        const saveEmailAnswer = await askInteractive(`Do you want to save the email "${email}"? `)
+
+        if(saveEmailAnswer === 'yes') {
+            try{
+                await saveEmail(email)
+                traceSuccess(`Email saved ${email}!`)
+            }catch(e){
+                traceException('Error saving email, try again!')
+            }
+        }else{
+            traceException('Has decided not to save email!')
+        }
+    }else {
+        traceException(`No exist user email.`)
+    }
+} 
+
+
+saveEmailInteractive()
